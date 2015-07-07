517147b2d8335c842f01cf63417a656e0fff1cde
Fabien-Chouteau <fabien.chouteau@gmail.com>
2015-07-07 21:40:39 +0200

    DOS to UNIX file format

--- src/text_utils.ads
+++ src/text_utils.ads
@@ -1,3 +1,24 @@
+-------------------------------------------------------------------------------
+--                                                                           --
+--                               Eagle Lander                                --
+--                                                                           --
+--         Copyright (C) 2015 Fabien Chouteau (chouteau@adacore.com)         --
+--                                                                           --
+--    Eagle Lander is free software: you can redistribute it and/or modify   --
+--    it under the terms of the GNU General Public License as published by   --
+--    the Free Software Foundation, either version 3 of the License, or      --
+--    (at your option) any later version.                                    --
+--                                                                           --
+--    Eagle Lander is distributed in the hope that it will be useful,        --
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of         --
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          --
+--    GNU General Public License for more details.                           --
+--                                                                           --
+--    You should have received a copy of the GNU General Public License      --
+--    along with Eagle Lander.  If not, see <http://www.gnu.org/licenses/>.  --
+--                                                                           --
+-------------------------------------------------------------------------------
+
 with Glib; use Glib;
 with Pango; use Pango;
 with Pango.Layout; use Pango.Layout;
